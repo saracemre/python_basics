ae687bdc6925b27af9a1839614a1c162815ca24a
saracemre <96650488+saracemre@users.noreply.github.com>
2024-12-06 10:28:36 +0100

    Delete Veri_Yapilari directory

--- Veri_Yapilari/bazi_denemeler.py
+++
@@ -1,7 +0,0 @@
-a = float(input("Dik ucgenin ilk dik kenari:"))
-
-b = float(input("Dik ucgenin ikinci dik kenari:"))
-
-c = (a ** 2 + b ** 2) ** (1/2)
-
-print("Hipotenus:", c)
--- Veri_Yapilari/comment.py
+++
@@ -1,3 +0,0 @@
-# // (tam bolen):10 sayisi 3'e bolunur 
-# ve tam kismi alinir.
-print(10//3) 
